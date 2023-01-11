923442a2fda1ce577346f9fcb7e9e953be09f3a4
juancortes1bx@hotmail.es <juancortes1bx@hotmail.es>
2023-01-11 12:39:42 +0100

    añadido atributos

--- src/TheWitcher/Witcher.java
+++ src/TheWitcher/Witcher.java
@@ -1,7 +1,10 @@
 package TheWitcher;
 
 public class Witcher {
-
+	private  String name;
+	private int health;
+	private boolean hasSoul;
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
